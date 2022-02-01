7d38c47b75c7cd58dc6907e7d37b327e9d7e0ec0
Theomanevit <theo.manevit@limayrac.fr>
2022-02-01 17:08:18 +0100

    2

--- accueil2.php
+++ accueil2.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>accueil</title>
+    <link rel="stylesheet" href="css/accueil2.css">
 </head>
 <body>
-    
+    <h3>Accueil</h3>
+    <form action="accueil2.php" method="post">
+        <input type="submit" value="Foire aux questions" />
+    </form>
+    <a href="#"><img src="img/personne.png" alt="compte" /></a>
+    <div>
+    <h1>
+        M2l
+    </h1>
+    </div>
 </body>
 </html>
--- connexion.php
+++ connexion.php
@@ -1,12 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>accueil</title>
+    <link rel="stylesheet" href="css/accueil.css">
 </head>
 <body>
-    
+    <h3>Accueil</h3>
+    <form action="connexion.php" method="post">
+        <input type="submit" value="Connextion" />
+        <input type="submit" value="inscription" />
+    </form>
+    <div>
+    <h1>
+        M2l
+    </h1>
+    <table>
+        <tr><td><form action="connexion.php" method="post">
+    <input type="text" name="pseudo" value="pseudo"/>
+    </form></td></tr>
+        <tr><td><form action="connexion.php" method="post">
+    <input type="text" name="mot de passe" value="mot de passe"/>
+    </form></td></tr>
+    </table>
+    <form action="connexion.php">
+        <input type="submit" value="annuler" />
+        <input type="submit" value="valider" />
+    </form>
+    </div>
 </body>
 </html>
