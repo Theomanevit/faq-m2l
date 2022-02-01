e5c6ac5490b5488d4fa0ca3d690fee14c87d1f04
BTracas <benjamin.tracas@limayrac.fr>
2022-02-01 17:00:59 +0100

    questions part 1.2

--- questions.php
+++ questions.php
@@ -4,19 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/questions.css">
     <title>Liste des questions</title>
 </head>
 <body>
-<a href="accueil2.php">
-<h1>Foire aux questions</h1><br/>
-<table>
-    <th>
-        question
-    </th>
+
+
+
+
+<a href="accueil.php"><img src="img/log_out.png" alt="log_out"/></a>
+
+
+
+<a href="accueil2.php">Accueil</a><a href="ajout_question.php">ajouter une question</a></div><br/>
+<h1>Foire aux questions</h1>
+
+<table>   
+<td element class="bord"><table>
+       <tr><td><p>question</p></td></tr>
+        <tr><td><p>Réponse</p></td></tr>
+</table><a href="modif_question.php">Modifier</a><a href="suppr_question.php">supprimer</a></td>
 </table>
-
-
-
 
 
 </body>
