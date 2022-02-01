78f73a10c1d6a8b8f4160cd2498609f63c5b8d00
BTracas <benjamin.tracas@limayrac.fr>
2022-02-01 17:00:22 +0100

    part 1 questions

--- questions.php
+++ questions.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Liste des questions</title>
 </head>
 <body>
-    
+<a href="accueil2.php">
+<h1>Foire aux questions</h1><br/>
+<table>
+    <th>
+        question
+    </th>
+</table>
+
+
+
+
+
 </body>
 </html>
