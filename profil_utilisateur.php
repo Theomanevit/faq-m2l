e574716dc651791b9d2a9884d5349c1bff9259a9
DWalkiewiez <dorian.walkiewiez@limayrac.fr>
2022-02-08 14:19:19 +0100

    hutifolfvoiuklvjlcv

--- profil_utilisateur.php
+++ profil_utilisateur.php
@@ -4,30 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/profil.css">
     <title>profil utilisateur</title>
 </head>
 <body>
  <center><h1>profil utilisateur</h1></center>
-<a href="Accueil.php"><input type="button"value="Accueil"/></a>
-<center><a href="questions.php"><input type="button"value="FAQ"/></a></center>
-<div align="right"><a href="déconnexion.php"><input type="button"value="se deconnecter"/></a></div>
-<center><TABLE>
-<TR>
-<TD>pseudo</TD>
-<TD>user name</TD>
-</TR>
-<TR>
-<TD>adresse mail</TD>
-<TD>user mail</TD>
-</TR>
-<TR>
-<TD>mot de passe</TD>
-<TD>***********</TD>
-</TR>
-<TR>
-<TD>ligue</TD>
-<TD>ligue name</TD>
-</TR>
-</TABLE></center>
+<a href="Accueil.php"><input type="button"value="Accueil"/></a><center><a href="questions.php"><input type="button"value="FAQ"/></a></center><div align="right"><a href="déconnexion.php"><input type="button"value="se deconnecter"/></a></div>
+<center><table>
+<tr>
+<td>pseudo</td>
+<td>user name</td>
+</tr>
+<tr>
+<td>adresse mail</td>
+<td>user mail</td>
+</tr>
+<tr>
+<td>mot de passe</td>
+<td>***********</td>
+</tr>
+<tr>
+<td>ligue</td>
+<td>ligue name</td>
+</tr>
+</table></center>
 </body>
 </html>
