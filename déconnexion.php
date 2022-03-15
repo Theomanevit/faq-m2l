2c93fd61ed213980c790b1a9424f510a5dc7c055
Theomanevit <theo.manevit@limayrac.fr>
2022-03-15 16:56:24 +0100

    1

--- déconnexion.php
+++ déconnexion.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,17 +8,26 @@
     <title>déconnection</title>
     <link rel="stylesheet" href="css/déco.css">
 </head>
+
 <body>
 
     <div>
-    <h1>
-        M2l
-    </h1>
-    <center><table>
-        <tr><td><h2>Déconnexion</h2><h3>Voulez-vous vraiment vous déconnecter ?</h3></td></tr>
-    </table></center>
-    <center><a href="index.php"><input type="submit" value="oui" /></a>
-        <a href="accueil2.php"><input type="submit" value="non" /></a></center>
+        <h1>
+            M2l
+        </h1>
+        <table>
+            <tr>
+                <td>
+                    <h2>Déconnexion</h2>
+                    <h3>Voulez-vous vraiment vous déconnecter ?</h3>
+                </td>
+            </tr>
+        </table>
+        <center>
+            <a href="index.php"><input type="submit" value="oui" /></a>
+            <a href="accueil2.php"><input type="submit" value="non" /></a>
+        </center>
     </div>
 </body>
+
 </html>
