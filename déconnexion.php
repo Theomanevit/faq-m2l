26c56d344baa49b30a4dd3571441cbd6c17ae242
DWalkiewiez <dorian.walkiewiez@limayrac.fr>
2022-03-15 15:12:10 +0100

    page déconnexion qui marche

--- déconnexion.php
+++ déconnexion.php
@@ -5,23 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>déconnection</title>
-    <link rel="stylesheet" href="css/accueil2.css">
+    <link rel="stylesheet" href="css/déco.css">
 </head>
 <body>
-    <h3>Accueil</h3>
-    <a href="#"><img src="img/personne.png" alt="compte" /></a>
-    <a href="#"><img src="img/log_out.png" alt="log_out" /></a>
+
     <div>
     <h1>
         M2l
     </h1>
-    <table>
-        <tr><td><p>Déconnexion</p><p>Voulez-vous vraiment vous déconnecter ?</p></td></tr>
-    </table>
-    <form action="connexion.php">
-        <input type="submit" value="oui" />
-        <input type="submit" value="non" />
-    </form>
+    <center><table>
+        <tr><td><h2>Déconnexion</h2><h3>Voulez-vous vraiment vous déconnecter ?</h3></td></tr>
+    </table></center>
+    <center><a href="index.php"><input type="submit" value="oui" /></a>
+        <a href="accueil2.php"><input type="submit" value="non" /></a></center>
     </div>
 </body>
 </html>
