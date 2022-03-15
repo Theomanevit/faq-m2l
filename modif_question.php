b23aa3c46eeab84926571a70f1dc71cc7bcb2e67
Theomanevit <theo.manevit@limayrac.fr>
2022-03-15 14:16:53 +0100

    entete

--- modif_question.php
+++ modif_question.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/questions.css">
-    <title>Document</title>
+    <title>modification question</title>
 </head>
 
 <body>
