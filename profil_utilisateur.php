0e63b36383c74f61174fb61bcddce611189f5922
Theomanevit <theo.manevit@limayrac.fr>
2022-03-15 16:44:19 +0100

    1

--- profil_utilisateur.php
+++ profil_utilisateur.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,30 +8,37 @@
     <link rel="stylesheet" href="css/profil.css">
     <title>profil utilisateur</title>
 </head>
+
 <body>
- <center><h1>profil utilisateur</h1></center>
- <center><nav><a class="boutondroite" href="accueil2.php"> <input type="button" value="accueil"> </a>
-    <a class="boutondroite" href="questions.php"><input type="button" value="FAQ"></a>
-    <a class="boutondroite" href="déconnexion.php"><input type="button" value="se déconnecter"></a></nav><center>
-<center><table>   
-<td element class="bord"><table>
-<tr>
-<td>pseudo</td>
-<td>user name</td>
-</tr>
-<tr>
-<td>adresse mail</td>
-<td>user mail</td>
-</tr>
-<tr>
-<td>mot de passe</td>
-<td>***********</td>
-</tr>
-<tr>
-<td>ligue</td>
-<td>ligue name</td>
-</tr>
-</table>
-</table></center>
+    <h1>profil utilisateur</h1>
+    <center>
+        <a href="accueil2.php" class="centre">accueil</a>
+        <a class="centre" href="questions.php">FAQ</a>
+        <a class="centre" href="déconnexion.php">se déconnecter</a>
+    </center>
+    <p></p>
+
+    <table>
+        <td element class="bord">
+            <table>
+                <tr>
+                    <td>pseudo</td>
+                    <td>user name</td>
+                </tr>
+                <tr>
+                    <td>adresse mail</td>
+                    <td>user mail</td>
+                </tr>
+                <tr>
+                    <td>mot de passe</td>
+                    <td>***********</td>
+                </tr>
+                <tr>
+                    <td>ligue</td>
+                    <td>ligue name</td>
+                </tr>
+            </table>
+    </table>
 </body>
+
 </html>
