a7f21adfe24793509201df4de368e7d95187f963
Theomanevit <theo.manevit@limayrac.fr>
2022-03-15 14:05:40 +0100

    modif

--- accueil2.php
+++ accueil2.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>accueil</title>
+    <title>accueil2</title>
     <link rel="stylesheet" href="css/accueil2.css">
 </head>
 <body>
