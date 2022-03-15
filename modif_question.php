22ed9429ddc541268292e4db205d652d7cc9911e
Theomanevit <theo.manevit@limayrac.fr>
2022-03-15 14:15:53 +0100

    1

--- modif_question.php
+++ modif_question.php
@@ -1,20 +1,11 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
 <html lang="en">
-=======
-<html lang="en">
-
->>>>>>> 2b789cf0eed0730570f4a169f497813652155c60
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>Document</title>
-=======
     <link rel="stylesheet" href="css/questions.css">
     <title>Document</title>
->>>>>>> 2b789cf0eed0730570f4a169f497813652155c60
 </head>
 
 <body>
