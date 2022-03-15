44150e106cb8ac8108f0539e9af359f323e2a024
BTracas <benjamin.tracas@limayrac.fr>
2022-03-15 16:45:15 +0100

    déconnexion sur modif et suppr question

--- suppression_question.php
+++ suppression_question.php
@@ -14,7 +14,7 @@
         <h1>
             <a href="accueil2.php"><img class="image" src="img/home.png" alt="accueil" /></a>
             Foire aux questions
-            <a href="accueil.php"><img class="logout" src="img/log_out.png" alt="log_out" /></a>
+            <a href="déconnexion.php"><img class="logout" src="img/log_out.png" alt="log_out" /></a>
         </h1>
 
         <table class="tableaucentre">
