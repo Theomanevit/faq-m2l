bffe6957431e506bdcbf85df58577f7b8a13b479
Theomanevit <theo.manevit@limayrac.fr>
2022-03-22 15:03:51 +0100

    1

--- inscription.php
+++ inscription.php
@@ -1,12 +1,38 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=faq m2l';
+$user = 'root';
+$password = '';
+try {
+  $dbh = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  return $dbh;
+} catch (PDOException $ex) {
+  die("Erreur lors de la connexion SQL : " . $ex->getMessage());
+}
 
+$pseudo_uti = isset($_POST['pseudo_uti']) ? $_POST['pseudo_uti'] : NULL;
+$mail_uti = isset($_POST['mail_uti']) ? $_POST['mail_uti'] : NULL;
+$mdp_uti = isset($_POST['mdp_uti']) ? $_POST['mdp_uti'] : NULL;
+$id_ligue = isset($_POST['id_ligue']) ? $_POST['id_ligue'] : NULL;
+$id_type = isset($_POST['id_type']) ? $_POST['id_type'] : NULL;
+$submit = isset($_POST['submit']);
 
-
-
-
-
-
-
-
+if ($submit) {
+    $sql = "INSERT INTO utilisateur(pseudo_uti , mail_uti , mdp_uti , id_ligue, id_type)";
+    $sql. = "VALUES (:pseudo_uti , :mail_uti , :mdp_uti , :id_ligue, :id_type)";
+    try{
+        $sth = $dbh->prepare($sql);
+        $sth->execute(array(
+            ':pseudo_uti' => $pseudo_uti,
+            ':mail_uti' => $mail_uti,
+            ':mdp_uti' => $mdp_uti,
+            ':id_ligue' => $id_ligue,
+            ':id_type' => $id_type
+        ));
+    }catch (PDOException $ex) {
+        die("Erreur lors de la requête SQL : ".$ex->getMessage());
+    }echo "<p>".$sth->rowCount()." enregistrement(s) ajouté(s)</p>";
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
