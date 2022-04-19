e936dbf05586102d037cadf842262ebc7a4348c6
Theomanevit <theo.manevit@limayrac.fr>
2022-04-19 14:23:16 +0200

    14

--- questions.php
+++ questions.php
@@ -11,10 +11,24 @@ try {
 } catch (PDOException $ex) {
   die("Erreur lors de la connexion SQL : " . $ex->getMessage());
 }
+
+$pseudo_uti = isset($_GET['pseudo_uti']) ? $_GET['pseudo_uti'] : '';
+
 try {
   $sql = "select pseudo_uti,id_questions,lib_questions,date_questions,reponse,date_reponse from questions, utilisateur where questions.id_utilisateur=utilisateur.id_utilisateur";
   $sth = $dbh->prepare($sql);
   $sth->execute(array());
+  $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $ex) {
+  die("<p>Erreur lors de la requête SQL : " . $ex->getMessage() . "</p>");
+}
+
+try {
+  $sql = "select id_type from utilisateur where pseudo_uti=:pseudo_uti";
+  $sth = $dbh->prepare($sql);
+  $sth->execute(array(
+    ':pseudo_uti' => $pseudo_uti
+  ));
   $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $ex) {
   die("<p>Erreur lors de la requête SQL : " . $ex->getMessage() . "</p>");
@@ -46,10 +60,12 @@ try {
         echo "<br><table class='tableauligne'><tr><th><p>" . $row["id_questions"] . "</p></th><th><p>" . $row["pseudo_uti"] . "</p></th></tr>";
         echo "<tr><td><p>" . $row["date_questions"] . "</p></td><td><p>" . $row["lib_questions"] . "</p></td></tr>";
         echo "<tr><td><p>" . $row["date_reponse"] . "</p></td><td><p>" . $row["reponse"] . "</p></td></tr>";
-        echo "</table>";  
-        echo '<td><a href="reponse_question.php?id_questions=' . $row['id_questions'] . '"><img class="image" src="img/reponse.png" alt="edit"></a></td>';
-        echo '<td><a href="modif_question.php?id_questions=' . $row['id_questions'] . '"><img class="image" src="img/edit.png" alt="edit"></a></td>';
-        echo '<td><a href="suppression_question.php?id_questions=' . $row['id_questions'] . '"><img class="image" src="img/trash.png" alt="edit"></a><br><br></td>';
+        echo "</table>";
+        if($row['id_type']==2 ||$row['id_type']==3){
+        }
+        echo '<a href="reponse_question.php?id_questions=' . $row['id_questions'] . '"><img class="image" src="img/reponse.png" alt="edit"></a>';
+        echo '<a href="modif_question.php?id_questions=' . $row['id_questions'] . '"><img class="image" src="img/edit.png" alt="edit"></a>';
+        echo '<a href="suppression_question.php?id_questions=' . $row['id_questions'] . '"><img class="image" src="img/trash.png" alt="edit"></a><br><br>';
       }
     } else {
       echo '<p> Désolé mais vous ne pouvez pas accèder à cette page! </p>';
